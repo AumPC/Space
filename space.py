52432061d75c77ec60b4e94a367e119f7ca93ddd
AumPC <blue-aum@hotmail.com>
2016-12-21 20:52:08 +0700

    modified:   space.py 	__pycache__/ 	models.py

--- space.py
+++ space.py
@@ -1,22 +1,26 @@
 import arcade
- 
+from models import Ship,World
+
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
  
 class SpaceGameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
- 
         arcade.set_background_color(arcade.color.BLACK)
  
-        self.ship = arcade.Sprite('images/ship.png')
-        self.ship.set_position(100, 100)
- 
+        self.ship_sprite = arcade.Sprite('images/ship.png')
+        self.world = World(width,height)
  
     def on_draw(self):
         arcade.start_render()
+        self.ship_sprite.draw()
  
-        self.ship.draw()
+    def animate(self, delta):
+        self.world.animate(delta)
+        self.ship_sprite.set_position(self.world.ship.x, self.world.ship.y)
+
+        
  
  
 if __name__ == '__main__':
