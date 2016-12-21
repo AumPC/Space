844d1a22ec110bf89ae06a6acfa461a6758544d9
AumPC <blue-aum@hotmail.com>
2016-12-21 21:24:22 +0700

    modified:   __pycache__/models.cpython-35.pyc 	modified:   models.py 	modified:   space.py

--- space.py
+++ space.py
@@ -3,24 +3,43 @@ from models import Ship,World
 
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
+
+class ModelSprite(arcade.Sprite):
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop('model', None)
+ 
+        super().__init__(*args, **kwargs)
+ 
+    def sync_with_model(self):
+        if self.model:
+            self.set_position(self.model.x, self.model.y)
+ 
+    def draw(self):
+        self.sync_with_model()
+        self.angle = self.model.angle
+        super().draw()
+
+
  
 class SpaceGameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
         arcade.set_background_color(arcade.color.BLACK)
- 
-        self.ship_sprite = arcade.Sprite('images/ship.png')
+
         self.world = World(width,height)
- 
+        self.ship_sprite = ModelSprite('images/ship.png',model=self.world.ship)
+        self.gold_sprite = ModelSprite('images/gold.png',model=self.world.gold)
+        
     def on_draw(self):
         arcade.start_render()
+        self.gold_sprite.draw()
         self.ship_sprite.draw()
  
     def animate(self, delta):
         self.world.animate(delta)
-        self.ship_sprite.set_position(self.world.ship.x, self.world.ship.y)
 
-        
+    def on_key_press(self, key, key_modifiers):
+        self.world.on_key_press(key, key_modifiers)
  
  
 if __name__ == '__main__':
